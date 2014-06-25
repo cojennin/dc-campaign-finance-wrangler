1b06bc5b4111d8e339d374156d358c1b649b0d3b
DONALD BRAMAN <dbraman@law.gwu.edu>
2014-06-25 01:41:33 -0400

    removed index from csv creation and added 1999_current csv files

--- app.py
+++ app.py
@@ -21,30 +21,51 @@ import requests_cache
 requests_cache.install_cache(allowable_methods=['GET', 'POST'], expire_after=60 * 60 * 6)
 
 
-# download the data, put it into pandas, & save to  csv files for hand inspection & review
 data_dir = '../data'
 if not os.path.exists(data_dir):
     os.makedirs(data_dir)
 currentYear = datetime.now().year
+
+# Download data from 2000 on, just for storage and review.
+# We can't do all the cool stuff with it because we don't
+# have data on who ran for what office yet. But we should get that.
+
+start_date = '01/01/1999'                           # set the start datae
+end_date = '12/31/' + str(currentYear)                # set the end date
+
+contributions = pd.read_csv(
+    StringIO.StringIO(dc_campaign_finance_data.scraper.records_csv(start_date, end_date, 'con')))  # con = contributions
+filename = os.path.join(data_dir, 'contributions_1999_current.csv')
+contributions.to_csv(filename, index=False)
+
+expenditures = pd.read_csv(
+    StringIO.StringIO(dc_campaign_finance_data.scraper.records_csv(start_date, end_date, 'exp')))  # exp = expenditures
+filename = os.path.join(data_dir, 'expenditures_1999_current.csv')
+expenditures.to_csv(filename, index=False)
+
+
+# download the data, put it into pandas, & save to  csv files for hand inspection & review
 start_date = '01/01/2010'                           # set the start datae
 end_date = '12/31/' + str(currentYear)                # set the end date
+
 print datetime.now(), 'downloading offices'
 offices = dc_campaign_finance_data.scraper.offices()    # get list of offices for all years
-print datetime.now(), 'downloading contributions'
 # Load data into pandas & convert dollars to float
+print datetime.now(), 'downloading contributions'
 contributions = pd.read_csv(
     StringIO.StringIO(dc_campaign_finance_data.scraper.records_csv(start_date, end_date, 'con')),
     converters={'Amount': lambda x: float(x.replace('$', '').replace(',', '').replace('(', '').replace(')', ''))})  # con = contributions
 filename = os.path.join(data_dir, 'contributions_2010_current.csv')
-contributions.to_csv(filename)
-print datetime.now(), 'downloading expenditures'
+contributions.to_csv(filename, index=False)
 # Load data into pandas & convert dollars to float
+print datetime.now(), 'downloading expenditures'
 expenditures = pd.read_csv(
     StringIO.StringIO(dc_campaign_finance_data.scraper.records_csv(start_date, end_date, 'exp')),
     converters={'Amount': lambda x: float(x.replace('$', '').replace(',', '').replace('(', '').replace(')', ''))})  # exp = expenditures
 filename = os.path.join(data_dir, 'expenditures_2010_current.csv')
-expenditures.to_csv(filename)
+expenditures.to_csv(filename, index=False)
 print datetime.now(), 'data loaded!'
+
 
 
 # generate a json file with the open offices for each year
@@ -72,6 +93,7 @@ with open(filename, 'w') as outfile:
     json.dump(json_out, outfile)
 
 # generate json files containing annual contributions and expenditures by year by office by candidate
+# this is also annoying because it has to be merged with committee and office names which have to be downloaded for each year.
 
 for year in range(2010, currentYear + 1):
     print datetime.now(), 'generating contributions for', year, 'offices:'
