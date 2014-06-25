de5c3327cd9d559f4f99cabaeb8c4489840bb33f
DONALD BRAMAN <dbraman@law.gwu.edu>
2014-06-25 00:07:40 -0400

    pep8-ified

--- app.py
+++ app.py
@@ -17,57 +17,53 @@ import collections
 import json
 import requests
 import requests_cache
-requests_cache.install_cache(allowable_methods = ['GET', 'POST'], expire_after = 60*60*6)
+requests_cache.install_cache(allowable_methods=['GET', 'POST'], expire_after=60 * 60 * 6)
 
 
-# ## first we grab the data & put into local files for munging
+# first we grab the data & put into local files for munging
 data_dir = '../data'
 if not os.path.exists(data_dir):
     os.makedirs(data_dir)
 currentYear = datetime.now().year
 start_date = '01/01/2010'                           # set the start datae
-end_date = '12/31/'+str(currentYear)                # set the end date
+end_date = '12/31/' + str(currentYear)                # set the end date
 print datetime.now(), 'downloading offices'
 offices = dc_campaign_finance_data.scraper.offices()    # get list of offices for all years
 print datetime.now(), 'downloading contributions'
-contributions_data = dc_campaign_finance_data.scraper.records_csv(start_date, end_date, 'con') # con = contributions
+contributions_data = dc_campaign_finance_data.scraper.records_csv(start_date, end_date, 'con')  # con = contributions
 print datetime.now(), 'downloading expenditures'
 expenditures_data = dc_campaign_finance_data.scraper.records_csv(start_date, end_date, 'exp')  # exp = expenditures
 print datetime.now(), 'downloading done!'
 
 
-## In order to make the data more useful in our pivot tables,
-## we need to add a columng that indicates for which office each candidate is running.
-## These offices are matched with committee names by the OCF, so that's what we'll use.
+# In order to make the data more useful in our pivot tables,
+# we need to add a columng that indicates for which office each candidate is running.
+# These offices are matched with committee names by the OCF, so that's what we'll use.
 
-## Load data into pandas & convert dollars to float
+# Load data into pandas & convert dollars to float
 
 print datetime.now(), 'loading contributions into pandas'
 filename = os.path.join(data_dir, 'contributions_2010_current.csv')
-contributions = pd.read_csv(filename, converters={'Amount': lambda x: float(x.replace('$', '').replace(',','').replace('(','').replace(')',''))})
+contributions = pd.read_csv(filename, converters={'Amount': lambda x: float(x.replace('$', '').replace(',', '').replace('(', '').replace(')', ''))})
 contributions['Zip'] = contributions['Zip'].astype('object')
 contributions.to_csv(filename)
 
 print datetime.now(), 'loading expenditures into pandas'
 filename = os.path.join(data_dir, 'expenditures_2010_current.csv')
-expenditures = pd.read_csv(filename, converters={'Amount': lambda x: float(x.replace('$', '').replace(',','').replace('(','').replace(')',''))})
+expenditures = pd.read_csv(filename, converters={'Amount': lambda x: float(x.replace('$', '').replace(',', '').replace('(', '').replace(')', ''))})
 expenditures.to_csv(filename)
 
 print datetime.now(), 'data loaded!'
 
 
-# push the data into a few json files
-
-
+# generate a json file with the open offices for each year
+# this is a little annoying because it has to be downloaded for each year
 
 json_out = {}
 for year in range(2010, currentYear + 1):
-    print ' '
-    print datetime.now(), 'starting', year
-    datetime.now(), 'generating dataframe of committees running for', year, 'offices:'
+    print datetime.now(), 'generating list of committees running for', year, 'offices:'
     offices_df = pd.DataFrame(columns=['Year', 'Office', 'Committee Name'])
     for office in offices:
-        print year, 'downloading list of committees running for', office
         committee_list = dc_campaign_finance_data.scraper.committees(office, year)
         if len(committee_list) > 0:
             for committee in committee_list:
@@ -82,24 +78,20 @@ for year in range(2010, currentYear + 1):
     json_out.update(output_json)
 filename = os.path.join(data_dir, 'years and offices.json')
 with open(filename, 'w') as outfile:
-      json.dump(json_out, outfile)
+    json.dump(json_out, outfile)
 
-## Next we need to generate json files containing annual contributions and expenditures by year by office by candidate
+# generate json files containing annual contributions and expenditures by year by office by candidate
 
 for year in range(2010, currentYear + 1):
-    print year
-    datetime.now(), 'generating dataframe of committees running for', year, 'offices:'
+    print datetime.now(), 'generating contributions for', year, 'offices:'
     offices_df = pd.DataFrame(columns=['Committee Name', 'Office'])
     filename = os.path.join(data_dir, 'merged_' + str(year) + '_contributions.csv')
     merged = pd.read_csv(filename)
     for office in offices:
-        print datetime.now(), 'downloading list of committees running for', office
         committee_list = dc_campaign_finance_data.scraper.committees(office, year)
         if len(committee_list) > 0:
-            cyo = merged[ merged['Office'] == office ]
+            cyo = merged[merged['Office'] == office]
             cyo_table = pd.pivot_table(cyo, values='Amount', index=['Contributor Type'], columns=['Committee Name'], aggfunc=np.sum)
             cyo_df = pd.DataFrame(cyo_table)
             filename = os.path.join(data_dir, str(year) + ' ' + office + '.json')
-            cyo_df.to_json(path_or_buf = filename)
-
-
+            cyo_df.to_json(path_or_buf=filename)
