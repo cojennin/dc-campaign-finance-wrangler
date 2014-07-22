1ffb2ddde6dda1cb515da500224b8e12028339b9
donaldbraman <donald.braman@gmail.com>
2014-07-22 17:36:48 -0400

    switch to wrangler

--- download.py
+++ download.py
@@ -6,13 +6,7 @@ import os
 import time
 import datetime
 import io
-import dc_campaign_finance_scraper
-import requests
-import pandas as pd
-import numpy as np
-import pickle
-import collections
-import json
+from  dc_campaign_finance_scraper import scraper
 
 '''setup up input directory'''
 
@@ -24,30 +18,25 @@ if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
 '''
-DOWNLOAD CONTRIBUTIONS, EXPENDITURES, AND OFFICE DATA.
+DOWNLOAD CONTRIBUTIONS AND EXPENDITURES DATA.
 Even though we only have offices from 2000 on,
 we download from 1999 on, just for storage and review.
 We should eventually enter in the offices data for 1999-2009.
 '''
 
-start_date = '01/01/1999'            # set the start datae
-end_date = datetime.date.today().strftime("%m/%d/%Y")                # set the end date
+start_date = '01/01/1999'
+end_date = datetime.date.today().strftime("%m/%d/%Y")
 
-filename = os.path.join(input_dir, 'all_contributions_1999_current.csv')  # name of the file for conttributions
-contributions = dc_campaign_finance_scraper.records_json() # con = contributions
-contributions = pd.read_json(io.StringIO(contributions), parse_dates=True)
-contributions['Amount'] = contributions['Amount'].str.replace(',', '').str.replace('$', '').str.replace('(', '').str.replace(')', '').astype('float')
-contributions.to_csv(filename, index=False)
+# contributions = scraper.records_with_office_and_election_year(from_date=start_date, to_date=end_date, report_type='con')
+# filename = os.path.join(input_dir, 'contributions.csv')
+# with open(filename, 'w') as f:
+#     f.write(contributions.csv)
+# f.close()
 
-filename = os.path.join(input_dir, 'all_expenditures_1999_current.csv')
-expenditures = dc_campaign_finance_data.scraper.records_csv(start_date, end_date, 'exp')
-expenditures = pd.read_csv(io.StringIO(expenditures), parse_dates=True)  # exp = expenditures
-expenditures.to_csv(filename, index=False)
-
-filename = os.path.join(input_dir, 'all_offices.pkl')
-offices = dc_campaign_finance_data.scraper.offices()   # download a list of all offices
-# offices = [o.replace('\r\n', '') for o in offices]  # push them into a pickle
-with open(filename, 'wb') as f:
-    pickle.dump(offices, f)
+expenditures = scraper.records_with_office_and_election_year(from_date=start_date, to_date=end_date, report_type='exp')
+filename = os.path.join(input_dir, 'expenditures.csv')
+with open(filename, 'w') as f:
+    f.write(expenditures.csv)
+f.close()
 
 
---
+++ wrangle.py
@@ -0,0 +1,34 @@
+'''
+CLEAN AND WRAGLE DATA INTO JSON.
+'''
+
+import os
+import io
+import pandas as pd
+import numpy as np
+
+input_dir = '../data/input'
+output_dir = '../data/output'
+
+# read in data and clean up a bit
+filename = os.path.join(input_dir, 'contributions.csv')
+contributions = pd.read_csv(filename)
+contributions['Amount'] = contributions['Amount'].str.replace(',', '').str.replace('$', '').str.replace('(', '').str.replace(')', '').astype('float')
+contributions = contributions[contributions['Election Year'] > 0]
+contributions['Election Year'] = contributions['Election Year'].astype('int16')
+
+# make a list of offices for each election year
+yo = contributions[['Election Year', 'Office']]
+yo = yo.drop_duplicates()
+yo = yo.reset_index()
+yo = pd.DataFrame(yo[['Election Year', 'Office']])
+
+# output json files for graphs
+for rownum in range(0, len(yo.index)):
+    year = yo.iloc[rownum, 0]
+    office = yo.iloc[rownum, 1]
+    data_out = contributions[(contributions['Election Year'] ==  year)]
+    data_out = data_out[(data_out['Office'] ==  office)]
+    data_out = data_out[['Candidate Name', 'Contributor', 'Contributor Type', 'Address', 'city', 'state', 'Zip', 'Contribution Type', 'Amount', 'Date of Receipt']]
+    json_filename = os.path.join(output_dir, str(year) +' ' + str(office) + '.json')
+    data_out.to_json(json_filename, orient = 'records')
