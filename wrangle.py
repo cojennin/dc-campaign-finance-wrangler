b5f869494315dfcc41893b873f5c3199e5743fd7
donaldbraman <donald.braman@gmail.com>
2014-08-15 08:58:37 -0400

    adding new graphs

--- wrangle.py
+++ wrangle.py
@@ -1,5 +1,5 @@
 '''
-CLEAN AND WRAGLE DATA INTO JSON.
+CLEAN AND WRAGLE DATA INTO JSON
 '''
 
 import os
@@ -9,6 +9,8 @@ import numpy as np
 
 input_dir = '../dc-campaign-finance-data/csv'
 output_dir = '../dc-campaign-finance-data/json'
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
 
 # read in data and clean up a bit
 filename = os.path.join(input_dir, 'contributions.csv')
@@ -21,14 +23,58 @@ contributions['Election Year'] = contributions['Election Year'].astype('int16')
 yo = contributions[['Election Year', 'Office']]
 yo = yo.drop_duplicates()
 yo = yo.reset_index()
-yo = pd.DataFrame(yo[['Election Year', 'Office']])
+yo = pd.DataFrame(yo[['Election Year', 'Office']]).sort(['Election Year', 'Office'],ascending=[0,1])
+json_filename = os.path.join(output_dir, 'years and offices.json')
+yo.to_json(json_filename, orient = 'records')
 
-# output json files for graphs
+
+# json files for complex graphs
 for rownum in range(0, len(yo.index)):
     year = yo.iloc[rownum, 0]
     office = yo.iloc[rownum, 1]
-    data_out = contributions[(contributions['Election Year'] ==  year)]
-    data_out = data_out[(data_out['Office'] ==  office)]
-    data_out = data_out[['Candidate Name', 'Contributor', 'Contributor Type', 'Address', 'city', 'state', 'Zip', 'Contribution Type', 'Amount', 'Date of Receipt']]
+    data = contributions[(contributions['Election Year'] ==  year)]
+    data = data[(data['Office'] ==  office)]
+    data = data[['Candidate Name', 'Contributor', 'Contributor Type', 'Address', 'city', 'state', 'Zip', 'Contribution Type', 'Amount', 'Date of Receipt']]
     json_filename = os.path.join(output_dir, str(year) +' ' + str(office) + '.json')
-    data_out.to_json(json_filename, orient = 'records')
+    data.to_json(json_filename, orient = 'records')
+
+
+# json files for grass-roots graphs
+output_dir = '../dc-campaign-finance-data/json/grass-roots'
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+for rownum in range(0, len(yo.index)):
+    year = yo.iloc[rownum, 0]
+    office = yo.iloc[rownum, 1]
+    data = contributions[contributions['Election Year'] ==  year]
+    data = data[(data['Office'] ==  office) & (data['state'] ==  'DC') & (data['Contributor Type'] == 'Individual')]
+    data['unique'] = data['Contributor'] + data['Address']
+    data = data[['Candidate Name', 'unique']].drop_duplicates()
+    data['mycount'] = 1
+    data = data.groupby(['Candidate Name']).sum()
+    data = data.sort(['mycount'],ascending=False).reset_index()
+    data = pd.DataFrame(data[['Candidate Name', 'mycount']])
+    json_filename = os.path.join(output_dir, str(year) +' ' + str(office) + '.json')
+    data.to_json(json_filename, orient = 'records')
+
+# json files for corporate graphs
+output_dir = '../dc-campaign-finance-data/json/corporate'
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+for rownum in range(0, len(yo.index)):
+    year = yo.iloc[rownum, 0]
+    office = yo.iloc[rownum, 1]
+    data = contributions[contributions['Election Year'] ==  year]
+    data = data[data['Office'] ==  office]
+    data = data[(data['Contributor Type'] == 'Corporation') | (data['Contributor Type'] == 'Corporate Sponsored PAC') | (data['Contributor Type'] == 'Business')]
+    data['unique'] = data['Contributor'] + data['Address']
+    data = data[['Candidate Name', 'unique']].drop_duplicates()
+    data['mycount'] = 1
+    data = data.groupby(['Candidate Name']).sum()
+    data = data.sort(['mycount'],ascending=False).reset_index()
+    data = pd.DataFrame(data[['Candidate Name', 'mycount']])
+    json_filename = os.path.join(output_dir, str(year) +' ' + str(office) + '.json')
+    data.to_json(json_filename, orient = 'records')
+
+
+
