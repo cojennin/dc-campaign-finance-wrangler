341558f6c9224f5a797c8d031f7014cc7528273b
Connor Jennings <cojennin@gmail.com>
2014-09-22 00:13:50 -0400

    Some code cleanup to leverage s3.

--- scrape-ocf.py
+++ scrape-ocf.py
@@ -2,17 +2,9 @@
 # and generates various data tables suitable for graphing.
 # It uses @sshanabrook's awesome dc_campaign_finance_data package.
 
-import os
-import time
 import datetime
-import io
 from  dc_campaign_finance_scraper import scraper
-
-'''setup up input and output directories'''
-
-csv_dir = '../dc-campaign-finance-data/csv'
-if not os.path.exists(csv_dir):
-    os.makedirs(csv_dir)
+from simple_bucket import SimpleBucket
 
 '''
 DOWNLOAD CONTRIBUTIONS AND EXPENDITURES DATA.
@@ -23,18 +15,11 @@ We could eventually enter in the offices data for 1999-2009.
 
 start_date = '01/01/1990'
 end_date = datetime.date.today().strftime("%m/%d/%Y")
+prefix = "csv"
 
 contributions = scraper.records_with_office_and_election_year(from_date=start_date, to_date=end_date, report_type='con')
-filename = os.path.join(csv_dir, 'ocf_contributions.csv')
-with open(filename, 'w') as f:
-    f.write(contributions.csv)
-f.close()
-
 expenditures = scraper.records_with_office_and_election_year(from_date=start_date, to_date=end_date, report_type='exp')
-filename = os.path.join(csv_dir, 'ocf_expenditures.csv')
-with open(filename, 'w') as f:
-    f.write(expenditures.csv)
-f.close()
 
-
-
+bucket = SimpleBucket()
+bucket.save([prefix, "ocf-contributions.csv"], contributions)
+bucket.save([prefix, "ocf-expenditures.csv"], expenditures)
---
+++ simple_bucket.py
@@ -0,0 +1,16 @@
+import boto
+
+
+class SimpleBucket:
+    def __init__(self, aws_access_key_id, aws_secret_access_key, bucket_name, delim="/"):
+        #Sane defaults just in case.
+        self.connection = boto.connect_s3(aws_access_key_id, aws_secret_access_key)
+        self.bucket = self.connection.lookup(bucket_name)
+        self.delim = delim
+
+    def save(self, key_list, contents):
+        #Example output in S3 might look like dc-campaign-finance-ocf-data/csv/ofc_contributions.csv
+        s3_obj = self.bucket.new_key(self.delim.join(key_list))
+
+        #This could be a long running op, might be worthwhile to add a callback.
+        s3_obj.set_contents_from_string(contents)
