dd28c8d1eb10320aa4bc58ba50939a38cf3b6e41
Connor Jennings <cojennin@gmail.com>
2014-09-22 00:49:22 -0400

    More tweaking to use s3.

--- scrape-ocf.py
+++ scrape-ocf.py
@@ -3,8 +3,9 @@
 # It uses @sshanabrook's awesome dc_campaign_finance_data package.
 
 import datetime
-from  dc_campaign_finance_scraper import scraper
+from dc_campaign_finance_scraper import scraper
 from simple_bucket import SimpleBucket
+import yaml
 
 '''
 DOWNLOAD CONTRIBUTIONS AND EXPENDITURES DATA.
@@ -13,13 +14,23 @@ we download contribution and expenditure data from 1999 on for storage and revie
 We could eventually enter in the offices data for 1999-2009.
 '''
 
+#Could probably use some logical grouping
 start_date = '01/01/1990'
 end_date = datetime.date.today().strftime("%m/%d/%Y")
 prefix = "csv"
 
+#Get our configuration options
+config_file = open("config.yml", 'r')
+config = yaml.safe_load(config_file)
+config_file.close()
+
+print("Fetching contributions...")
 contributions = scraper.records_with_office_and_election_year(from_date=start_date, to_date=end_date, report_type='con')
+print("Fetching expenditures...")
 expenditures = scraper.records_with_office_and_election_year(from_date=start_date, to_date=end_date, report_type='exp')
 
-bucket = SimpleBucket()
+bucket = SimpleBucket(config['aws']['s3']['aws_key_id'],
+                      config['aws']['s3']['aws_secret_key'],
+                      config['aws']['s3']['bucket_name'])
 bucket.save([prefix, "ocf-contributions.csv"], contributions)
 bucket.save([prefix, "ocf-expenditures.csv"], expenditures)
